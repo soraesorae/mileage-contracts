907273606a88616e48b2518e6ca62c1d0b1ef331
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-10-18 12:47:28 +0900

    Add dependencies

--- src/SwMileageToken.sol
+++ src/SwMileageToken.sol
@@ -2,7 +2,25 @@
 pragma solidity ^0.8.13;
 
 import {KIP7} from "kaia-contracts/contracts/KIP/token/KIP7/KIP7.sol";
+import {IKIP7} from "kaia-contracts/contracts/KIP/token/KIP7/IKIP7.sol";
+import {KIP7Burnable} from "kaia-contracts/contracts/KIP/token/KIP7/extensions/KIP7Burnable.sol";
+import {Pausable} from "kaia-contracts/contracts/security/Pausable.sol";
+import {Ownable} from "kaia-contracts/contracts/access/Ownable.sol";
 
-contract SwMaileageToken is KIP7 {
-    constructor() KIP7("SwMileageToken", "SMT") {}
+contract SwMaileageToken is IKIP7, KIP7, KIP7Burnable, Pausable, Ownable {
+    /**
+     * @dev satisfy KIP13
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(KIP7, KIP7Burnable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @dev contract constructor
+     * set KIP7 token name, symbol
+     *
+     * @param name_ token name
+     * @param symbol_ token symbol
+     */
+    constructor(string memory name_, string memory symbol_) KIP7(name_, symbol_) {}
 }
