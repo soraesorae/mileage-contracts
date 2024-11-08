12d1641e2d5a217c63c213ff823a89789729543f
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-11-08 20:59:15 +0900

    Add Factory contract

---
+++ src/SwMileageFactory.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {SwMileageToken} from "./SwMileageToken.sol";
+
+contract SwMileageTokenFactory {
+    constructor() {}
+
+    function deploy(string memory _name, string memory _symbol) external returns (address) {
+        SwMileageToken token = new SwMileageToken(_name, _symbol);
+        token.transferOwnership(msg.sender);
+        return address(token);
+    }
+}
---
+++ test/SwMileageFactory.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SwMileageToken} from "../src/SwMileageToken.sol";
+import {SwMileageTokenFactory} from "../src/SwMileageFactory.sol";
+
+interface Ownable {
+    function owner() external view returns (address);
+}
+
+contract SwMileageFactoryTst is Test {
+    // SwMileageToken public mileageToken;
+    SwMileageTokenFactory public factory;
+    address alice = address(0x1234);
+
+    function setUp() public {
+        vm.prank(alice);
+        factory = new SwMileageTokenFactory();
+        console.log(address(this));
+        console.log(address(factory));
+    }
+
+    function test_Deploy() public {
+        vm.prank(alice);
+        SwMileageToken deployed = SwMileageToken(factory.deploy("AAAA", "BBBB"));
+        console.log(address(deployed));
+        assertEq(deployed.name(), "AAAA");
+        assertEq(deployed.symbol(), "BBBB");
+        assertEq(deployed.owner(), alice);
+    }
+}
