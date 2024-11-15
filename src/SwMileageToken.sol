aaf85aa837d4a91bd80da07a8c907275bd7b5d13
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-11-15 22:14:48 +0900

    Fix

--- src/SwMileageToken.sol
+++ src/SwMileageToken.sol
@@ -26,7 +26,7 @@ contract SwMileageToken is Context, IKIP7, KIP7, KIP7Burnable, Pausable, Ownable
     // constructor(string memory name_, string memory symbol_) KIP7(name_, symbol_) {}
     constructor(string memory name_, string memory symbol_) KIP7(name_, symbol_) {}
 
-    function decimals() public view override returns (uint8) {
+    function decimals() public pure override returns (uint8) {
         return 0;
     }
 
