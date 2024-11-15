fc67f7595d0a024928364c1b366c7c1a9c633675
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-11-15 22:15:57 +0900

    Fix factory contract

--- src/SwMileageFactory.sol
+++ src/SwMileageFactory.sol
@@ -3,14 +3,17 @@ pragma solidity ^0.8.13;
 
 import {SwMileageToken} from "./SwMileageToken.sol";
 
+// https://github.com/Uniswap/v2-core/blob/master/contracts/UniswapV2Factory.sol
 contract SwMileageTokenFactory {
+    event MileageTokenCreated(address token);
+
     constructor() {}
 
     function deploy(string memory _name, string memory _symbol) external returns (address) {
         SwMileageToken token = new SwMileageToken(_name, _symbol);
-        // token.transferOwnership(msg.sender);
         token.addOwnership(msg.sender);
         token.removeOwnership(address(this));
+        emit MileageTokenCreated(address(token));
         return address(token);
     }
 }
