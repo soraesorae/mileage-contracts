858c485bc7af87995160c17bc2f3a8b895e330a4
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-11-17 15:25:57 +0900

    Refactor factory contarct

--- src/SwMileageFactory.sol
+++ src/SwMileageFactory.sol
@@ -5,15 +5,24 @@ import {SwMileageToken} from "./SwMileageToken.sol";
 
 // https://github.com/Uniswap/v2-core/blob/master/contracts/UniswapV2Factory.sol
 contract SwMileageTokenFactory {
-    event MileageTokenCreated(address token);
+    event CreateMileageToken(address tokenAddress);
 
     constructor() {}
 
+<<<<<<< HEAD
     function deploy(string memory _name, string memory _symbol) external returns (address) {
         SwMileageToken token = new SwMileageToken(_name, _symbol);
         token.addOwnership(msg.sender);
         token.removeOwnership(address(this));
         emit MileageTokenCreated(address(token));
         return address(token);
+=======
+    function deploy(string memory name, string memory symbol) external returns (address) {
+        SwMileageToken mileageToken = new SwMileageToken(name, symbol);
+        mileageToken.addOwnership(msg.sender);
+        mileageToken.removeOwnership(address(this));
+        emit CreateMileageToken(address(mileageToken));
+        return address(mileageToken);
+>>>>>>> 8edc740 (Refactor factory contarct)
     }
 }
