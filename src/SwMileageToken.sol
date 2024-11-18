7ea17629c3bb3b69ef36ed71be09a0e9a5cc665e
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-11-18 19:13:50 +0900

    Prevent to call  AND

--- src/SwMileageToken.sol
+++ src/SwMileageToken.sol
@@ -5,25 +5,24 @@ import {KIP7} from "kaia-contracts/contracts/KIP/token/KIP7/KIP7.sol";
 import {IKIP7} from "kaia-contracts/contracts/KIP/token/KIP7/IKIP7.sol";
 import {KIP7Burnable} from "kaia-contracts/contracts/KIP/token/KIP7/extensions/KIP7Burnable.sol";
 import {Pausable} from "kaia-contracts/contracts/security/Pausable.sol";
-import {OwnableMulti} from "./OwnableMulti.sol";
+import {Admin} from "./Admin.sol";
 import {Context} from "kaia-contracts/contracts/utils/Context.sol";
 
 import {SortedList} from "./SortedList.sol";
 import {ISwMileageToken} from "./ISwMileageToken.sol";
 
 // TODO: new contract for multiple owner instead of `Ownable`
-contract SwMileageToken is Context, IKIP7, KIP7, KIP7Burnable, Pausable, OwnableMulti, SortedList {
+contract SwMileageToken is Context, IKIP7, KIP7, KIP7Burnable, Pausable, Admin, SortedList {
     struct Student {
-        address wallet;
+        address account;
         uint256 balance;
     }
+
     /// @dev contract constructor
     /// set KIP7 token name, symbol
     ///
     /// @param name_ token name
     /// @param symbol_ token symbol
-
-    // constructor(string memory name_, string memory symbol_) KIP7(name_, symbol_) {}
     constructor(string memory name_, string memory symbol_) KIP7(name_, symbol_) {}
 
     function decimals() public pure override returns (uint8) {
@@ -38,11 +37,11 @@ contract SwMileageToken is Context, IKIP7, KIP7, KIP7Burnable, Pausable, Ownable
         return super.supportsInterface(interfaceId);
     }
 
-    function pause() public onlyOwner {
+    function pause() public onlyAdmin {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public onlyAdmin {
         _unpause();
     }
 
@@ -50,7 +49,8 @@ contract SwMileageToken is Context, IKIP7, KIP7, KIP7Burnable, Pausable, Ownable
     ///
     /// @param _account `to` account
     /// @param _amount token amount
-    function mint(address _account, uint256 _amount) public onlyOwner whenNotPaused {
+    ///
+    function mint(address _account, uint256 _amount) public onlyAdmin whenNotPaused {
         _mint(_account, _amount);
     }
 
@@ -58,13 +58,24 @@ contract SwMileageToken is Context, IKIP7, KIP7, KIP7Burnable, Pausable, Ownable
     ///
     function burn(
         uint256 amount
-    ) public override {} // empty
+    ) public pure override {}
+    ////
+
+    function _approve(address, /* owner */ address, /* spender */ uint256 /* amount */ ) internal pure override {
+        require(false, "Blocked");
+    }
+
+    function _transfer(address, /* from */ address, /* to */ uint256 /* amount */ ) internal pure override {
+        require(false, "Blocked");
+    }
 
     /// @dev KIP7Burnable burnFrom
     /// bypass allowance check when msg.sender is owner
+    ///
     /// @param account target account
     /// @param amount amount
-    function burnFrom(address account, uint256 amount) public override onlyOwner whenNotPaused {
+    ///
+    function burnFrom(address account, uint256 amount) public override onlyAdmin whenNotPaused {
         // if (_msgSender() != owner()) {
         //     _spendAllowance(account, _msgSender(), amount);
         // }
@@ -75,10 +86,9 @@ contract SwMileageToken is Context, IKIP7, KIP7, KIP7Burnable, Pausable, Ownable
         require(from == address(0) || to == address(0), "only mint or burn");
     }
 
-    // check is secure
-    function _afterTokenTransfer(address from, address to, uint256 /*amount*/ ) internal override {
-        // TODO _update(..., INC) _update(..., DEC)
+    function _afterTokenTransfer(address from, address to, uint256 /* amount */ ) internal virtual override {
         // require(from == address(0) || to == address(0));
+        // increase decrease
         if (from == address(0)) {
             // mint
             _updateElement(to, balanceOf(to));
