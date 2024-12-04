10e28507715468e642615950d9ea90600327b643
soraesorae <165246201+soraesorae@users.noreply.github.com>
2024-12-04 22:28:01 +0900

    Refactor factory contract

--- src/SwMileageFactoryOwner.sol
+++ src/SwMileageFactoryOwner.sol
@@ -5,12 +5,24 @@ import {Ownable} from "kaia-contracts/contracts/access/Ownable.sol";
 import {SwMileageToken} from "./SwMileageToken.sol";
 
 // https://github.com/Uniswap/v2-core/blob/master/contracts/UniswapV2Factory.sol
-contract SwMileageTokenFactory {
+contract SwMileageTokenFactory is Ownable {
     event MileageTokenCreated(address tokenAddress);
+
+    bool private _on = true;
+
+    modifier on() {
+        require(_on, "off");
+        _;
+    }
 
     constructor() {}
 
-    function deploy(string memory name, string memory symbol) external returns (address) {
+    function off() external onlyOwner on {
+        renounceOwnership();
+        _on = false;
+    }
+
+    function deploy(string memory name, string memory symbol) external on returns (address) {
         SwMileageToken mileageToken = new SwMileageToken(name, symbol);
         mileageToken.addAdmin(msg.sender);
         mileageToken.removeAdmin(address(this));
