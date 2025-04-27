f90af00d80c17ae13a0c3ed1b1d095dc62681a19
soraesorae <165246201+soraesorae@users.noreply.github.com>
2025-04-27 11:06:26 +0900

    feat: add structure for changing account

--- src/IStudentManager.sol
+++ src/IStudentManager.sol
@@ -1,21 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {KIP7} from "kaia-contracts/contracts/KIP/token/KIP7/KIP7.sol";
-import {IKIP7} from "kaia-contracts/contracts/KIP/token/KIP7/IKIP7.sol";
-import {KIP7Burnable} from "kaia-contracts/contracts/KIP/token/KIP7/extensions/KIP7Burnable.sol";
-import {Pausable} from "kaia-contracts/contracts/security/Pausable.sol";
-import {Admin} from "./Admin.sol";
-import {Context} from "kaia-contracts/contracts/utils/Context.sol";
-
-import {SortedList} from "./SortedList.sol";
-import {ISwMileageToken} from "./ISwMileageToken.sol";
-
 interface IStudentManager {
     struct DocumentSubmission {
+        SubmissionStatus status;
         bytes32 studentId;
         bytes32 docHash;
-        DocStatus status;
+    }
+
+    struct AccountChangeRequest {
+        SubmissionStatus status;
+        bytes32 studentId;
+        address targetAccount;
     }
 
     struct DocumentResult {
@@ -23,15 +19,19 @@ interface IStudentManager {
         uint256 amount;
     }
 
-    enum DocStatus {
+    enum SubmissionStatus {
+        NotExists,
         Pending,
         Approved,
         Rejected
     }
 
-    event DocSubmitted(uint256 index, bytes32 studentId, bytes32 docHash);
-    event DocApproved(uint256 index, bytes32 studentID, uint256 amount);
+    event DocSubmitted(uint256 indexed index, bytes32 indexed studentId, bytes32 docHash);
+    event DocApproved(uint256 indexed index, bytes32 indexed studentId, uint256 amount);
     event DocRejected();
-    event AccChangeSubmitted();
-    event AccChangeApprove();
+    event AccountChangeRequested(uint256 indexed index, bytes32 indexed studentId, address current, address target);
+    event AccountChangeApproved(uint256 indexed index, bytes32 indexed studentId, address previous, address current);
+    event AccountChangeRejected();
+
+    event AccountChanged(bytes32 indexed studentId, address previous, address current);
 }
