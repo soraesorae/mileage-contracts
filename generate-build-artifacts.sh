8d945faf384bba8607bf205d12a253ec53795f76
soraesorae <165246201+soraesorae@users.noreply.github.com>
2025-05-22 23:45:02 +0900

    fix: add `mkdir -p` (#24)

--- generate-build-artifacts.sh
+++ generate-build-artifacts.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+ARTIFACTS_DIR="./artifacts"
+
 forge build --silent
+
+mkdir -p $ARTIFACTS_DIR
 
 jq '.bytecode.object' ./out/SwMileageToken.impl.sol/SwMileageTokenImpl.json | sed -r 's/^.{3}//' | sed -r 's/.{1}$//' > ./artifacts/SwMileageToken.bytecode.txt
 jq '.bytecode.object' ./out/StudentManager.impl.sol/StudentManagerImpl.json | sed -r 's/^.{3}//' | sed -r 's/.{1}$//' > ./artifacts/StudentManager.bytecode.txt
