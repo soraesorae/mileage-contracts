88aa15d9941b067f2187b2dac1f3e9efa9cac504
soraesorae <165246201+soraesorae@users.noreply.github.com>
2025-05-24 21:01:33 +0900

    fix: support factory contracts

--- generate-build-artifacts.sh
+++ generate-build-artifacts.sh
@@ -8,6 +8,10 @@ mkdir -p $ARTIFACTS_DIR
 
 jq '.bytecode.object' ./out/SwMileageToken.impl.sol/SwMileageTokenImpl.json | sed -r 's/^.{3}//' | sed -r 's/.{1}$//' > ./artifacts/SwMileageToken.bytecode.txt
 jq '.bytecode.object' ./out/StudentManager.impl.sol/StudentManagerImpl.json | sed -r 's/^.{3}//' | sed -r 's/.{1}$//' > ./artifacts/StudentManager.bytecode.txt
+jq '.bytecode.object' ./out/SwMileageFactory.sol/SwMileageTokenFactory.json  | sed -r 's/^.{3}//' | sed -r 's/.{1}$//' > ./artifacts/SwMileageTokenFactory.bytecode.txt
+jq '.bytecode.object' ./out/StudentManagerFactory.sol/StudentManagerFactory.json | sed -r 's/^.{3}//' | sed -r 's/.{1}$//' > ./artifacts/StudentManagerFactory.bytecode.txt
 
 jq '.abi' ./out/SwMileageToken.impl.sol/SwMileageTokenImpl.json > ./artifacts/SwMileageToken.abi.json
 jq '.abi' ./out/StudentManager.impl.sol/StudentManagerImpl.json > ./artifacts/StudentManager.abi.json
+jq '.abi' ./out/SwMileageFactory.sol/SwMileageTokenFactory.json > ./artifacts/SwMileageTokenFactory.abi.json
+jq '.abi' ./out/StudentManagerFactory.sol/StudentManagerFactory.json > ./artifacts/StudentManagerFactory.abi.json
